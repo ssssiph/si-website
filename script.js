e3570d14aebb0a111f2dab1b6b0270f479e3e7bc
ssssiph <alt.siph@gmail.com>
2025-05-22 13:03:40 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
     const links = document.querySelectorAll(".nav-links a");
     links.forEach(link => {
-        if (window.location.pathname.includes(link.getAttribute("href"))) {
+        if (window.location.pathname.endsWith("index.html") && link.getAttribute("href") === "index.html") {
+            link.classList.add("active");
+        }
+        if (window.location.pathname.endsWith("projects.html") && link.getAttribute("href") === "projects.html") {
             link.classList.add("active");
         }
     });
