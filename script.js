f6ea7d19c3eee60096f5ced9c43d9bb3356e177a
ssssiph <alt.siph@gmail.com>
2025-05-22 12:44:09 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
-    console.log("Навигация загружена!");
+    const links = document.querySelectorAll(".nav-links a");
+    links.forEach(link => {
+        if (window.location.href.includes(link.getAttribute("href"))) {
+            link.classList.add("active");
+        }
+    });
 });
