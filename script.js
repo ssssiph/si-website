fda8052c5de65c890f4b666fffd020577e695e7e
ssssiph <alt.siph@gmail.com>
2025-05-22 21:54:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const links = document.querySelectorAll(".nav-links a");
-    const currentPage = window.location.pathname.split("/").pop();
-    
-    links.forEach(link => {
-        link.classList.remove("active");
-        
-        if (currentPage === "" || currentPage === "index.html") {
-            if (link.getAttribute("href") === "index.html") {
-                link.classList.add("active");
-            }
-        }
-        
-        if (currentPage === "projects.html") {
-            if (link.getAttribute("href") === "projects.html") {
-                link.classList.add("active");
-            }
-        }
+    const projectContainer = document.querySelector(".projects-container");
+    const leftArrow = document.querySelector("#left-arrow");
+    const rightArrow = document.querySelector("#right-arrow");
+
+    let scrollAmount = 0;
+    const scrollStep = 300; // Длина прокрутки
+
+    leftArrow.addEventListener("click", () => {
+        scrollAmount -= scrollStep;
+        projectContainer.scrollTo({
+            left: scrollAmount,
+            behavior: "smooth"
+        });
+    });
+
+    rightArrow.addEventListener("click", () => {
+        scrollAmount += scrollStep;
+        projectContainer.scrollTo({
+            left: scrollAmount,
+            behavior: "smooth"
+        });
     });
 });
